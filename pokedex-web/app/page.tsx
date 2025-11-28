cdb32896a70410bf1e60e34ab4601a56d163ff09
Nabhan <jawahiirn@gmail.com>
2025-11-28 20:57:14 +0600

    chore: cleanup test console log

--- pokedex-web/app/page.tsx
+++ pokedex-web/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
     setMounted(true);
   }, []);
 
-
   return (
     <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
