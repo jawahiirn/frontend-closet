f5e68a72d5153627cd1b537149faea44facd386b
Nabhan <jawahiirn@gmail.com>
2025-12-01 22:58:54 +0600

    update: theme & prettier

--- pokedex-web/lib/constants/theme-constants.ts
+++ pokedex-web/lib/constants/theme-constants.ts
@@ -1,8 +1,5 @@
 export const themes = [
-    { name: "Light", value: "light", },
-    { name: "Dark", value: "dark" },
-    {
-        name: "Violet Bloom (Dark)",
-        value: "violet-bloom-dark",
-    }
+  { name: 'Light', value: 'light' },
+  { name: 'Dark', value: 'dark' },
+  { name: 'Violet Bloom (Dark)', value: 'violet-bloom-dark' },
 ];
